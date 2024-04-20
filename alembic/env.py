73c894e6d9975fbac353745bd95d807f3be68932
Money UrT <money.urt@gmail.com>
2024-04-20 08:48:36 -0700

    [wip] database

--- alembic/env.py
+++ alembic/env.py
@@ -7,7 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from urt30t import db, settings
+from urt30t import settings
+from urt30t.db._models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = db.Base.metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
