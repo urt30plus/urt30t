f2ad6a62853a0b88065ffff21066d8a193dc7934
Money UrT <money.urt@gmail.com>
2024-05-04 07:49:49 -0700

    [wip] database

--- urt30t/db/_core.py
+++ urt30t/db/_core.py
@@ -3,14 +3,14 @@ import logging
 
 import sqlalchemy as sa
 from sqlalchemy.ext.asyncio import (
-    AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
 
 from .. import settings
 from ..models import Group, Player
-from ._models import Alias, Client, Guid, IPAddress
+from ._models import Alias, Connection, Guid, IPAddress
+from ._models import Player as DBPlayer
 
 logger = logging.getLogger(__name__)
 
@@ -24,88 +24,100 @@ session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def sync_player(player: Player) -> None:
-    async with session_maker.begin() as session:
-        if client := await _find_client(session, player):
-            _sync_client_player_attrs(player, client)
-            async with asyncio.TaskGroup() as tg:
-                tg.create_task(_sync_client_guids(session, client, player))
-                tg.create_task(_sync_client_aliases(session, client, player))
-                tg.create_task(_sync_client_ip_addresses(session, client, player))
-        else:
-            client = await _create_client(session, player)
-            _sync_client_player_attrs(player, client)
+    if db_player := await _find_player(player):
+        if not player.db_id:
+            await _sync_player_connections(db_player.pid)
+        _sync_player_attrs(db_player, player)
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(_sync_player_guids(db_player, player))
+            tg.create_task(_sync_player_aliases(db_player, player))
+            tg.create_task(_sync_player_ip_addresses(db_player, player))
+    else:
+        db_player = await _create_player(player)
+        await _sync_player_connections(db_player.pid)
+        _sync_player_attrs(db_player, player)
 
 
-def _sync_client_player_attrs(player: Player, client: Client) -> None:
-    player.db_id = client.cid
-    player.group = Group(client.level)
-    player.xp = client.xp
-
-
-async def _find_client(session: AsyncSession, player: Player) -> Client | None:
-    if player.auth:
-        stmt1 = sa.select(Client).where(Client.authl == player.auth)
-        result1 = await session.execute(stmt1)
-        # TODO: lookup guid if not set on player
-        return result1.scalar_one_or_none()
-    if player.guid:
-        stmt2 = sa.select(Guid).where(Guid.cl_guid == player.guid)
-        result2 = await session.execute(stmt2)
-        if db_guid := result2.scalar_one_or_none():
-            player.auth = db_guid.client.authl
-            return db_guid.client
+async def _find_player(player: Player) -> DBPlayer | None:
+    async with session_maker() as session:
+        if player.auth:
+            stmt1 = sa.select(DBPlayer).where(DBPlayer.auth == player.auth)
+            result1 = await session.execute(stmt1)
+            # TODO: lookup guid if not set on player
+            return result1.scalar_one_or_none()
+        if player.guid:
+            stmt2 = sa.select(Guid).where(Guid.guid == player.guid)
+            result2 = await session.execute(stmt2)
+            if db_guid := result2.scalar_one_or_none():
+                player.auth = db_guid.player.auth
+                return db_guid.player
     return None
 
 
-async def _sync_client_guids(
-    session: AsyncSession, client: Client, player: Player
-) -> None:
+def _sync_player_attrs(db_player: DBPlayer, player: Player) -> None:
+    player.db_id = db_player.pid
+    player.group = Group(db_player.group)
+    player.xp = db_player.xp
+
+
+async def _sync_player_guids(db_player: DBPlayer, player: Player) -> None:
     if not player.guid:
         return
-    stmt = sa.select(Guid).where(Guid.cid == client.cid, Guid.cl_guid == player.guid)
-    result = await session.execute(stmt)
-    if client_guid := result.scalar_one_or_none():
-        logger.error("Client %s has guid %s", client.cid, player.guid)
-        client_guid.use_count += 1
-    else:
-        session.add(Guid(cid=client.cid, cl_guid=player.guid))
+    async with session_maker.begin() as session:
+        stmt = sa.select(Guid).where(
+            Guid.pid == db_player.pid, Guid.guid == player.guid
+        )
+        result = await session.execute(stmt)
+        if db_guid := result.scalar_one_or_none():
+            db_guid.use_count += 1
+        else:
+            session.add(Guid(pid=db_player.pid, guid=player.guid))
 
 
-async def _sync_client_aliases(
-    session: AsyncSession, client: Client, player: Player
-) -> None:
-    stmt = sa.select(Alias).where(Alias.cid == client.cid, Alias.alias == player.name)
-    result = await session.execute(stmt)
-    if client_alias := result.scalar_one_or_none():
-        logger.error("Client %s has alias %s", client.cid, player.name)
-        client_alias.use_count += 1
-    else:
-        session.add(Alias(cid=client.cid, alias=player.name))
+async def _sync_player_aliases(db_player: DBPlayer, player: Player) -> None:
+    async with session_maker.begin() as session:
+        stmt = sa.select(Alias).where(
+            Alias.pid == db_player.pid, Alias.alias == player.name
+        )
+        result = await session.execute(stmt)
+        if db_alias := result.scalar_one_or_none():
+            db_alias.use_count += 1
+        else:
+            session.add(Alias(pid=db_player.pid, alias=player.name))
 
 
-async def _sync_client_ip_addresses(
-    session: AsyncSession, client: Client, player: Player
-) -> None:
+async def _sync_player_ip_addresses(db_player: DBPlayer, player: Player) -> None:
     if player.ip_address is None:
         return
     stmt = sa.select(IPAddress).where(
-        IPAddress.cid == client.cid, IPAddress.ip_address == player.ip_address
+        IPAddress.pid == db_player.pid, IPAddress.address == player.ip_address
     )
-    result = await session.execute(stmt)
-    if client_ip_address := result.scalar_one_or_none():
-        logger.error("Client %s has IP address %s", client.cid, player.ip_address)
-        client_ip_address.use_count += 1
-    else:
-        session.add(IPAddress(cid=client.cid, ip_address=player.ip_address))
+    async with session_maker.begin() as session:
+        result = await session.execute(stmt)
+        if db_ip_address := result.scalar_one_or_none():
+            db_ip_address.use_count += 1
+        else:
+            session.add(IPAddress(pid=db_player.pid, address=player.ip_address))
 
 
-async def _create_client(session: AsyncSession, player: Player) -> Client:
-    client = Client(authl=player.auth)
-    client.aliases = [Alias(alias=player.name)]
+async def _sync_player_connections(pid: int) -> None:
+    async with session_maker.begin() as session:
+        session.add(Connection(pid=pid))
+
+
+async def _create_player(player: Player) -> DBPlayer:
+    db_player = DBPlayer(
+        auth=player.auth,
+        guid=player.guid,
+        name=player.name,
+        ip_address=player.ip_address,
+    )
+    db_player.aliases = [Alias(alias=player.name)]
     if player.guid is not None:
-        client.cl_guids = [Guid(cl_guid=player.guid)]
+        db_player.guids = [Guid(guid=player.guid)]
     if player.ip_address is not None:
-        client.ip_addresses = [IPAddress(ip_address=player.ip_address)]
-    session.add(client)
-    await session.flush()
-    return client
+        db_player.ip_addresses = [IPAddress(address=player.ip_address)]
+    async with session_maker.begin() as session:
+        session.add(db_player)
+        await session.flush()
+        return db_player
--- urt30t/db/_models.py
+++ urt30t/db/_models.py
@@ -15,84 +15,97 @@ class Base(DeclarativeBase):
     pass
 
 
-class Client(Base):
-    __tablename__ = "urt30t_clients"
+class Player(Base):
+    __tablename__ = "urt30t_players"
 
-    cid: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    authl: Mapped[str] = mapped_column(sa.String(32), index=True, unique=True)
-    level: Mapped[int] = mapped_column(default=models.Group.GUEST.value)
+    pid: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    auth: Mapped[str] = mapped_column(sa.String(32), index=True, unique=True)
+    guid: Mapped[str] = mapped_column(sa.String(32), index=True, unique=True)
+    name: Mapped[str] = mapped_column(sa.String(32))
+    ip_address: Mapped[str] = mapped_column(sa.String(64))
+    group: Mapped[int] = mapped_column(default=models.Group.GUEST.value)
     xp: Mapped[float] = mapped_column(default=0.0)
     created_at: Mapped[datetime.datetime] = mapped_column(insert_default=sa.func.now())
     updated_at: Mapped[datetime.datetime] = mapped_column(
         insert_default=sa.func.now(), onupdate=sa.func.now()
     )
-    cl_guids: Mapped[list["Guid"]] = relationship(back_populates="client")
-    aliases: Mapped[list["Alias"]] = relationship(back_populates="client")
-    ip_addresses: Mapped[list["IPAddress"]] = relationship(back_populates="client")
+
+    guids: Mapped[list["Guid"]] = relationship(back_populates="player")
+    aliases: Mapped[list["Alias"]] = relationship(back_populates="player")
+    ip_addresses: Mapped[list["IPAddress"]] = relationship(back_populates="player")
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(cid={self.cid}, authl={self.authl})"
+        return f"{self.__class__.__qualname__}(pid={self.pid}, auth={self.auth})"
 
 
 class Guid(Base):
     __tablename__ = "urt30t_guids"
 
-    cid: Mapped[int] = mapped_column(sa.ForeignKey(Client.cid), primary_key=True)
-    cl_guid: Mapped[str] = mapped_column(sa.String(32), primary_key=True)
+    pid: Mapped[int] = mapped_column(sa.ForeignKey(Player.pid), primary_key=True)
+    guid: Mapped[str] = mapped_column(sa.String(32), primary_key=True)
     use_count: Mapped[int] = mapped_column(sa.Integer(), insert_default=1)
     created_at: Mapped[datetime.datetime] = mapped_column(insert_default=sa.func.now())
-    client: Mapped[Client] = relationship(back_populates="cl_guids")
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        insert_default=sa.func.now(), onupdate=sa.func.now()
+    )
+
+    player: Mapped[Player] = relationship(back_populates="guids")
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(cid={self.cid}, cl_guid={self.cl_guid})"
+        return f"{self.__class__.__qualname__}(pid={self.pid}, guid={self.guid})"
 
 
 class Alias(Base):
     __tablename__ = "urt30t_aliases"
 
-    cid: Mapped[int] = mapped_column(sa.ForeignKey(Client.cid), primary_key=True)
+    pid: Mapped[int] = mapped_column(sa.ForeignKey(Player.pid), primary_key=True)
     alias: Mapped[str] = mapped_column(sa.String(32), primary_key=True)
     use_count: Mapped[int] = mapped_column(sa.Integer(), insert_default=1)
     created_at: Mapped[datetime.datetime] = mapped_column(insert_default=sa.func.now())
-    client: Mapped[Client] = relationship(back_populates="aliases")
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        insert_default=sa.func.now(), onupdate=sa.func.now()
+    )
+
+    player: Mapped[Player] = relationship(back_populates="aliases")
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(cid={self.cid}, alias={self.alias})"
+        return f"{self.__class__.__qualname__}(pid={self.pid}, alias={self.alias})"
 
 
 class IPAddress(Base):
     __tablename__ = "urt30t_ip_addresses"
 
-    cid: Mapped[int] = mapped_column(sa.ForeignKey(Client.cid), primary_key=True)
-    ip_address: Mapped[str] = mapped_column(sa.String(64), primary_key=True)
+    pid: Mapped[int] = mapped_column(sa.ForeignKey(Player.pid), primary_key=True)
+    address: Mapped[str] = mapped_column(sa.String(64), primary_key=True)
     use_count: Mapped[int] = mapped_column(sa.Integer(), insert_default=1)
     created_at: Mapped[datetime.datetime] = mapped_column(insert_default=sa.func.now())
-    client: Mapped[Client] = relationship(back_populates="ip_addresses")
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        insert_default=sa.func.now(), onupdate=sa.func.now()
+    )
+
+    player: Mapped[Player] = relationship(back_populates="ip_addresses")
 
     def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__qualname__}"
-            f"(cid={self.cid}, ip_address={self.ip_address})"
-        )
+        return f"{self.__class__.__qualname__}" f"(cid={self.pid}, ip={self.address})"
 
 
 class Connection(Base):
     __tablename__ = "urt30t_connections"
 
-    cid: Mapped[int] = mapped_column(sa.ForeignKey(Client.cid), primary_key=True)
+    pid: Mapped[int] = mapped_column(sa.ForeignKey(Player.pid), primary_key=True)
     created_at: Mapped[datetime.datetime] = mapped_column(
         insert_default=sa.func.now(), primary_key=True
     )
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(cid={self.cid}@{self.created_at})"
+        return f"{self.__class__.__qualname__}({self.pid}@{self.created_at})"
 
 
 class Penalty(Base):
     __tablename__ = "urt30t_penalties"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    cid: Mapped[int] = mapped_column(sa.ForeignKey(Client.cid))
+    pid: Mapped[int] = mapped_column(sa.ForeignKey(Player.pid))
     kind: Mapped[models.Penalty] = mapped_column(sa.Enum(models.Penalty, length=20))
     reason: Mapped[str | None] = mapped_column(sa.String(255))
     duration: Mapped[int | None]
@@ -104,4 +117,7 @@ class Penalty(Base):
     )
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__qualname__}(id={self.id}, kind={self.kind})"
+        return (
+            f"{self.__class__.__qualname__}"
+            f"(id={self.id}, pid={self.pid}, kind={self.kind})"
+        )
