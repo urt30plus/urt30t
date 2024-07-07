f07667f1f2bfb462be116f87856e4a854c0dba9e
Money UrT <money.urt@gmail.com>
2024-07-07 08:35:07 -0700

    add option to clean out cache directories before running checks

--- run_checks.sh
+++ run_checks.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
+if [[ $1 == "clean" ]]; then
+    echo "cleaning all .*cache/ directories"
+    rm -rf .*cache/
+fi
+
 echo "CI is set to [${CI}]"
 if [[ $CI != "true" ]]; then
     pre-commit run --all-files
