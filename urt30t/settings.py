a0f6494a74492a802115d86754c05708a38635f3
Money UrT <money.urt@gmail.com>
2024-12-27 07:34:37 -0800

    update min version specifiers and only install tzdata where needed (Windows)

--- urt30t/settings.py
+++ urt30t/settings.py
@@ -17,7 +17,7 @@ from pydantic import (
     model_validator,
 )
 
-__version__ = "2024.11.26"
+__version__ = "2024.12.27"
 
 PACKAGE_ROOT = Path(__file__).parent
 PROJECT_ROOT = PACKAGE_ROOT.parent
