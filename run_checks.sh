0cbd96d32efc7a70f762fa7e27965e4f8b6d425a
Money UrT <money.urt@gmail.com>
2026-05-16 07:44:48 -0700

    switch from pre-commit to prek

--- run_checks.sh
+++ run_checks.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 set -xe
 
-if [[ -d .venv/bin ]]; then
-    export PATH=.venv/bin:$PATH
-fi
+export UV_FROZEN=1
 
-echo "CI is set to [${CI}]"
-if [[ $CI != "true" ]]; then
-    pre-commit run --all-files
-fi
-
-ty --version
-ty check
+uv run --active prek run --all-files
+uv run --active ty check
 
 export URT30T_CONFIG_FILE="./tests/test-config.toml"
 
-pytest -p no:cacheprovider
+uv run --active pytest -p no:cacheprovider
